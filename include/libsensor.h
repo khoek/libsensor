e3e1b42b72b08b3339d095c1fec3c988356ad648
Keeley Hoek <keeley@hoek.io>
2021-07-27 23:26:20 +1000

    Add support for inbound messages

--- include/libsensor.h
+++ include/libsensor.h
@@ -6,39 +6,66 @@
 #include <freertos/queue.h>
 #include <stdint.h>
 
-typedef enum poll_result {
-    POLL_RESULT_MADE_PROGRESS,
-    POLL_RESULT_UNEVENTFUL,
-    POLL_RESULT_FAIL,
-} poll_result_t;
+typedef enum sensor_poll_result {
+    SENSOR_POLL_RESULT_MADE_PROGRESS,
+    SENSOR_POLL_RESULT_UNEVENTFUL,
+    SENSOR_POLL_RESULT_FAIL,
+} sensor_poll_result_t;
 
-typedef void (*sensor_dev_fn_t)(void *dev);
-typedef poll_result_t (*sensor_poll_fn_t)(const char *tag, void *dev, QueueHandle_t queue);
+typedef void (*sensor_dev_start_fn_t)(void *dev, void *ctx);
+typedef void (*sensor_dev_reset_fn_t)(void *dev);
+
+typedef sensor_poll_result_t (*sensor_poll_fn_t)(const char *tag, void *dev, QueueHandle_t queue);
 typedef cJSON *(*sensor_report_fn_t)(const char *tag, void *dev, void *item);
 
+typedef void *(*sensor_ctx_init_fn_t)();
+typedef void (*sensor_ctx_destroy_fn_t)(void *ctx);
+typedef void (*sensor_recv_json_fn_t)(const char *tag, void *dev, void *ctx, const cJSON *json);
+
 typedef struct sensor_type {
+    ///// Data members, must be set.
+
     const char *name;
 
     size_t queue_item_size;
     uint32_t poll_delay_ms;
     size_t max_uneventful_iters;
 
-    sensor_dev_fn_t dev_start;
-    sensor_dev_fn_t dev_reset;
-    sensor_dev_fn_t dev_destroy;
+    ///// Data members, may be left unset (hence initialized to zero) with reasonable defaults used
+
+    size_t queue_length;
+    size_t initial_max_uneventful_iters;
+
+    size_t poll_task_stack_size;
+    size_t report_task_stack_size;
+
+    ///// Function members, cannot be null unless specified.
+
+    sensor_dev_start_fn_t dev_start;
+    // May be NULL.
+    sensor_dev_reset_fn_t dev_reset;
 
     sensor_poll_fn_t poll;
     sensor_report_fn_t report;
 
-    // The following members have reasonable defaults if set to zero.
-    size_t queue_length;
-    size_t poll_task_stack_size;
-    size_t report_task_stack_size;
+    // May be NULL.
+    sensor_ctx_init_fn_t ctx_init;
+    // May be NULL. It is guarenteed to be called after all other calls which pass
+    // the context have completed.
+    sensor_ctx_destroy_fn_t ctx_destroy;
+    // May be NULL.
+    sensor_recv_json_fn_t recv_json;
 } sensor_type_t;
 
-esp_err_t libsensor_create(const sensor_type_t *type, const char *tag, void *dev);
+typedef struct sensor_info *sensor_handle_t;
 
-// FIXME FIXME FIXME add a destroy function which returns the device handle (and waits for the poller thread to stop)!
-// (and thus we won't destroy it in the actual code)
+void libsensor_init();
+
+esp_err_t libsensor_register(const sensor_type_t *type, const char *tag, void *dev, sensor_handle_t *out_handle);
+
+// Returns the `dev` object captured by the original call to `libsensor_register`.
+void *libsensor_unregister(sensor_handle_t handle);
+
+void libsensor_dispatch_mqtt_message(const char *topic, size_t topic_len, const char *payload, size_t payload_len);
 
 #endif
--- src/libsensor/libsensor.c
+++ src/libsensor/libsensor.c
@@ -2,46 +2,85 @@
 
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
-#include <freertos/semphr.h>
+#include <freertos/event_groups.h>
 #include <libesp.h>
 #include <libiot.h>
 #include <string.h>
 
 static const char* TAG = "libsensor";
 
-#define POLL_TASK_PRIORITY 10
-#define REPORT_TASK_PRIORITY 5
+#define DISPATCH_TASK_PRIORITY 15
+#define DISPATCH_TASK_STACK_SIZE 4096
 
+#define DISPATCH_QUEUE_LENGTH 16
+
+#define POLL_TASK_PRIORITY 10
 #define DEFAULT_POLL_TASK_STACK_SIZE 2048
+
+#define REPORT_TASK_PRIORITY 5
 #define DEFAULT_REPORT_TASK_STACK_SIZE 4096
 
-#define DEFAULT_QUEUE_LENGTH 16
+#define DEFAULT_REPORT_QUEUE_LENGTH 16
+
+#define MAX_SENSOR_COUNT 16
+
+#define IN_TOPIC_PREFIX_BUFF_LEN 64
+
+#define MQTT_SENSOR_OUT_PREFIX "sensor-out"
+#define MQTT_SENSOR_IN_PREFIX "sensor-in"
 
 typedef struct sensor_info {
     const sensor_type_t* type;
     void* dev;
+    void* ctx;
 
     QueueHandle_t result_queue;
     char* tag;
     char* mqtt_topic;
 
-    // Used to stop both tasks, since when the `poll_task` stops
-    // is signals the `report_task` to stop.
-    SemaphoreHandle_t poll_task_should_stop;
-
-    // Should only be given by the `poll_task`, once `dev` has been
-    // destroyed and the task promises to no longer access this structure.
-    SemaphoreHandle_t report_task_should_stop;
+    // Used to stop both the poller and the reporter tasks.
+    EventGroupHandle_t events;
+#define SENSOR_INFO_EVENT_POLL_TASK_SHOULD_STOP (1ULL << 0)
+#define SENSOR_INFO_EVENT_REPORT_TASK_SHOULD_STOP (1ULL << 1)
+#define SENSOR_INFO_EVENT_REPORT_TASK_STOPPED (1ULL << 2)
 } sensor_info_t;
 
-// DANGER: This function does not free `info->dev`.
+typedef struct sensor_msg {
+    // The path is of the form '<sensor_type>/<sensor_tag>'.
+    char* path;
+    char* payload;
+} sensor_msg_t;
+
+static volatile bool global_initialized = false;
+
+static QueueHandle_t global_dispatch_queue;
+static StaticQueue_t global_dispatch_queue_static;
+static uint8_t global_dispatch_queue_buff[DISPATCH_QUEUE_LENGTH * sizeof(sensor_msg_t)];
+
+static SemaphoreHandle_t global_state_lock;
+static StaticSemaphore_t global_state_lock_static;
+static size_t global_sensor_count = 0;
+static sensor_info_t* global_sensor_list[MAX_SENSOR_COUNT];
+
+static char global_sensor_in_topic_prefix[IN_TOPIC_PREFIX_BUFF_LEN];
+static size_t global_sensor_in_topic_prefix_len;
+
+static void task_dispatch(void* unused);
+static void task_report(void* arg);
+static void task_poll(void* arg);
+
+// DANGER: This function does not free `info->events`.
 static void free_sensor_info(sensor_info_t* info) {
+    if (info->type->ctx_destroy) {
+        info->type->ctx_destroy(info->ctx);
+    }
+
     vQueueDelete(info->result_queue);
     free(info->tag);
     free(info->mqtt_topic);
 
-    vQueueDelete(info->poll_task_should_stop);
-    vQueueDelete(info->report_task_should_stop);
+    // Note that we cannot delete `info->events` at this point, since
+    // the task which caused the stop may still be waiting on it.
 
     free(info);
 }
@@ -51,7 +90,7 @@ static void begin_stopping_report_task(const sensor_info_t* info) {
     // since the reporter thread may free it, with the exception that we will
     // send a single null transmission on the result queue in order to wake
     // the reporter task.
-    xSemaphoreGive(info->report_task_should_stop);
+    xEventGroupSetBits(info->events, SENSOR_INFO_EVENT_REPORT_TASK_SHOULD_STOP);
 
     uint8_t buff[info->type->queue_item_size];
     memset(buff, 0, sizeof(buff));
@@ -59,32 +98,105 @@ static void begin_stopping_report_task(const sensor_info_t* info) {
         ;
 }
 
-static void task_report(void* arg);
-static void task_poll(void* arg);
+static void begin_stopping_and_wait_for_poll_task(sensor_info_t* info) {
+    EventGroupHandle_t events = info->events;
+
+    // DANGER: After this call, `info` may be freed.
+    xEventGroupSetBits(events, SENSOR_INFO_EVENT_POLL_TASK_SHOULD_STOP);
+
+    while (!xEventGroupWaitBits(events, SENSOR_INFO_EVENT_REPORT_TASK_STOPPED, pdFALSE, pdFALSE, portMAX_DELAY))
+        ;
+
+    vEventGroupDelete(events);
+}
+
+void libsensor_init() {
+    assert(!global_initialized);
+
+    global_state_lock = xSemaphoreCreateMutexStatic(&global_state_lock_static);
+    global_dispatch_queue = xQueueCreateStatic(DISPATCH_QUEUE_LENGTH,
+                                               sizeof(sensor_msg_t),
+                                               global_dispatch_queue_buff,
+                                               &global_dispatch_queue_static);
+
+    assert(xTaskCreate(&task_dispatch, "libsensor_dispatch", DISPATCH_TASK_STACK_SIZE, NULL, DISPATCH_TASK_PRIORITY, NULL) == pdPASS);
+
+    libiot_mqtt_build_local_topic_from_suffix(global_sensor_in_topic_prefix, sizeof(global_sensor_in_topic_prefix), MQTT_SENSOR_IN_PREFIX);
+    global_sensor_in_topic_prefix_len = strlen(global_sensor_in_topic_prefix);
+
+    while (xSemaphoreTake(global_state_lock, portMAX_DELAY) != pdTRUE)
+        ;
+
+    global_initialized = true;
+
+    xSemaphoreGive(global_state_lock);
+}
+
+static void add_sensor(sensor_info_t* info) {
+    while (xSemaphoreTake(global_state_lock, portMAX_DELAY) != pdTRUE)
+        ;
+
+    sensor_info_t** spot = &global_sensor_list[global_sensor_count];
+
+    global_sensor_count++;
+    assert(global_sensor_count < MAX_SENSOR_COUNT);
+
+    *spot = info;
+
+    xSemaphoreGive(global_state_lock);
+}
+
+static void remove_sensor(sensor_info_t* info) {
+    while (xSemaphoreTake(global_state_lock, portMAX_DELAY) != pdTRUE)
+        ;
+
+    size_t i = 0;
+
+    for (; i < global_sensor_count; i++) {
+        if (global_sensor_list[i] == info) {
+            break;
+        }
+    }
+
+    assert(i < global_sensor_count);
+    i++;
+
+    for (; i < global_sensor_count; i++) {
+        global_sensor_list[i - 1] = global_sensor_list[i];
+    }
+
+    global_sensor_count--;
+
+    xSemaphoreGive(global_state_lock);
+}
 
 // FIXME check these error paths
-// Note that if this function fails than it guarentees that `dev` has been destroyed
-// by the time it returns.
-esp_err_t libsensor_create(const sensor_type_t* type, const char* tag, void* dev) {
+esp_err_t libsensor_register(const sensor_type_t* type, const char* tag, void* dev, sensor_handle_t* out_handle) {
+    assert(global_initialized);
+
     sensor_info_t* info = malloc(sizeof(sensor_info_t));
     info->type = type;
     info->dev = dev;
+    info->ctx = NULL;
+    if (type->ctx_init) {
+        assert(type->ctx_destroy);
+        info->ctx = type->ctx_init();
+    }
 
     info->result_queue = NULL;
     info->tag = strdup(tag);
     info->mqtt_topic = NULL;
 
-    info->poll_task_should_stop = xSemaphoreCreateBinary();
-    info->report_task_should_stop = xSemaphoreCreateBinary();
+    info->events = xEventGroupCreate();
 
-    if (asprintf(&info->mqtt_topic, "sensor-%s-%s", type->name, tag) == -1) {
+    if (asprintf(&info->mqtt_topic, MQTT_SENSOR_OUT_PREFIX "/%s/%s", type->name, tag) == -1) {
         libiot_logf_error(TAG, "string allocation error");
         goto libsensor_sensor_create_fail_before_tasks;
     }
 
     size_t queue_length = type->queue_length;
     if (!queue_length) {
-        queue_length = DEFAULT_QUEUE_LENGTH;
+        queue_length = DEFAULT_REPORT_QUEUE_LENGTH;
     }
 
     // Note: it is the job of the reporter task to free the `sensor_info_t` struct, including
@@ -122,21 +234,38 @@ esp_err_t libsensor_create(const sensor_type_t* type, const char* tag, void* dev
         goto libsensor_sensor_create_fail_no_poll;
     }
 
+    add_sensor(info);
+
+    if (out_handle) {
+        *out_handle = info;
+    }
+
     return ESP_OK;
 
 libsensor_sensor_create_fail_before_tasks:
-    type->dev_destroy(dev);
+    vEventGroupDelete(info->events);
     free_sensor_info(info);
 
     return ESP_FAIL;
 
 libsensor_sensor_create_fail_no_poll:
     // It is to late to just free the `info` struct, since the reporter task can see it.
-    // Instead we have to do the poll task's shutdown errands for it.
-    type->dev_destroy(dev);
+    // Instead we have to do the poll task's shutdown errands for it, and then wait for
+    // the reporter task to stop.
     begin_stopping_report_task(info);
+    begin_stopping_and_wait_for_poll_task(info);
 
     return ESP_FAIL;
+}
+
+void* libsensor_unregister(sensor_info_t* info) {
+    void* dev = info->dev;
+
+    remove_sensor(info);
+    // DANGER: After this call, `info` may be freed.
+    begin_stopping_and_wait_for_poll_task(info);
+
+    return dev;
 }
 
 static void task_report(void* arg) {
@@ -152,7 +281,7 @@ static void task_report(void* arg) {
         // Has this task been instructed to stop, and have we just recieved the last message? (The latter check
         // is not a race because once the semaphore `info->report_task_should_stop` we promise to never send
         // anything on `info->result_queue`.)
-        if (uxSemaphoreGetCount(info->report_task_should_stop) && !uxQueueMessagesWaiting(info->result_queue)) {
+        if ((xEventGroupGetBits(info->events) & SENSOR_INFO_EVENT_REPORT_TASK_SHOULD_STOP) && !uxQueueMessagesWaiting(info->result_queue)) {
             // The last message ever sent to the queue is zeros, ignore it and break from this loop.
             break;
         }
@@ -167,14 +296,16 @@ static void task_report(void* arg) {
                 continue;
             }
 
-            libiot_mqtt_publish_local(info->mqtt_topic, 2, 0, msg);
+            libiot_mqtt_enqueue_local(info->mqtt_topic, 2, 0, msg);
             free(msg);
         }
+
+        ESP_ERROR_CHECK(util_stack_overflow_check());
     }
 
+    xEventGroupSetBits(info->events, SENSOR_INFO_EVENT_REPORT_TASK_STOPPED);
     free_sensor_info((void*) info);
 
-    ESP_ERROR_CHECK(util_stack_overflow_check());
     vTaskDelete(NULL);
 }
 
@@ -183,29 +314,38 @@ static void task_report(void* arg) {
 // reset and polling retried (in the case of an error/timeout), and is false
 // otherwise (if the thread itself has been asked to stop).
 static bool poll_loop(const sensor_info_t* info) {
+    size_t max_uneventful_iters = info->type->initial_max_uneventful_iters;
+    if (!max_uneventful_iters) {
+        max_uneventful_iters = info->type->max_uneventful_iters;
+    }
+
     size_t consecutive_uneventful_iters = 0;
     while (1) {
         // First check whether this thread has been instructed to stop.
-        if (xSemaphoreTake(info->poll_task_should_stop, 0) == pdTRUE) {
+        if (xEventGroupGetBits(info->events) & SENSOR_INFO_EVENT_POLL_TASK_SHOULD_STOP) {
             return false;
         }
 
         // If not, poll the sensor once, and process the result.
-        poll_result_t result = info->type->poll(info->tag, info->dev, info->result_queue);
+        sensor_poll_result_t result = info->type->poll(info->tag, info->dev, info->result_queue);
         switch (result) {
-            case POLL_RESULT_MADE_PROGRESS: {
+            case SENSOR_POLL_RESULT_MADE_PROGRESS: {
+                // Change the `max_uneventful_iters` limit from the initial limit
+                // (if there was one) to the normal limit.
+                max_uneventful_iters = info->type->max_uneventful_iters;
+
                 consecutive_uneventful_iters = 0;
                 break;
             }
-            case POLL_RESULT_UNEVENTFUL: {
+            case SENSOR_POLL_RESULT_UNEVENTFUL: {
                 consecutive_uneventful_iters++;
-                if (consecutive_uneventful_iters > info->type->max_uneventful_iters) {
+                if (consecutive_uneventful_iters > max_uneventful_iters) {
                     libiot_logf_error(TAG, "poll_loop() gave up due to uneventfulness, caused by: %s-%s", info->type->name, info->tag);
                     return true;
                 }
                 break;
             }
-            case POLL_RESULT_FAIL: {
+            case SENSOR_POLL_RESULT_FAIL: {
                 libiot_logf_error(TAG, "poll_loop() gave up due to explicit fail, caused by: %s-%s", info->type->name, info->tag);
                 return true;
             }
@@ -214,6 +354,8 @@ static bool poll_loop(const sensor_info_t* info) {
                 abort();
             }
         }
+
+        ESP_ERROR_CHECK(util_stack_overflow_check());
 
         if (info->type->poll_delay_ms) {
             vTaskDelay(1 + (info->type->poll_delay_ms / portTICK_PERIOD_MS));
@@ -228,19 +370,117 @@ static void task_poll(void* arg) {
 
     bool should_retry = true;
     while (should_retry) {
-        info->type->dev_start(info->dev);
+        info->type->dev_start(info->dev, info->ctx);
 
         should_retry = poll_loop(info);
 
-        info->type->dev_reset(info->dev);
+        if (info->type->dev_reset) {
+            info->type->dev_reset(info->dev);
+        }
     }
-
-    info->type->dev_destroy(info->dev);
 
     // At this point we are declaring that we will never access `info` again,
     // since the reporter thread may free it.
     begin_stopping_report_task(info);
 
-    ESP_ERROR_CHECK(util_stack_overflow_check());
     vTaskDelete(NULL);
+}
+
+static void handle_sensor_msg(const sensor_msg_t* msg) {
+    const char* path = msg->path;
+
+    cJSON* json = cJSON_Parse(msg->payload);
+    if (!json) {
+        libiot_logf_error(TAG, "JSON parse error for sensor path: %s", msg->path);
+        return;
+    }
+
+    while (xSemaphoreTake(global_state_lock, portMAX_DELAY) != pdTRUE)
+        ;
+
+    for (size_t i = 0; i < global_sensor_count; i++) {
+        sensor_info_t* sensor = global_sensor_list[i];
+        size_t name_len = strlen(sensor->type->name);
+
+        // Is the sensor name a prefix of the `path`?
+        if (strncmp(sensor->type->name, path, name_len) != 0) {
+            continue;
+        }
+
+        // If so, again advance `path` past the sensor name.
+        path += name_len;
+        if (*path != '/') {
+            continue;
+        }
+        path++;
+
+        // Finally, do the tags match?
+        if (strcmp(sensor->tag, path) != 0) {
+            continue;
+        }
+
+        // If so, dispatch the message.
+        if (!sensor->type->recv_json) {
+            libiot_logf_error(TAG, "dropping message for sensor '%s/%s' with no JSON handler", sensor->type->name, sensor->tag);
+            continue;
+        }
+
+        sensor->type->recv_json(sensor->tag, sensor->dev, sensor->ctx, json);
+    }
+
+    xSemaphoreGive(global_state_lock);
+
+    cJSON_Delete(json);
+}
+
+static void task_dispatch(void* unused) {
+    while (1) {
+        sensor_msg_t msg;
+        while (xQueueReceive(global_dispatch_queue, &msg, portMAX_DELAY) != pdTRUE)
+            ;
+
+        handle_sensor_msg(&msg);
+
+        free(msg.path);
+        free(msg.payload);
+        ESP_ERROR_CHECK(util_stack_overflow_check());
+    }
+
+    vTaskDelete(NULL);
+}
+
+void libsensor_dispatch_mqtt_message(const char* topic, size_t topic_len, const char* payload, size_t payload_len) {
+    assert(global_initialized);
+
+    // First check that `topic` has `global_sensor_in_topic_prefix` as a prefix.
+    size_t i = 0;
+    for (; i < topic_len && i < global_sensor_in_topic_prefix_len; i++) {
+        if (topic[i] != global_sensor_in_topic_prefix[i]) {
+            return;
+        }
+    }
+
+    if (i >= topic_len || topic[i] != '/') {
+        return;
+    }
+
+    // If so, advance `topic` past the prefix so that it is (presumably) of the form "<sensor_name>/<sensor_tag>".
+    const char* unterm_path = topic + global_sensor_in_topic_prefix_len + 1;
+    size_t unterm_path_len = topic_len - (global_sensor_in_topic_prefix_len + 1);
+
+    sensor_msg_t msg = {
+        .path = strndup(unterm_path, unterm_path_len),
+        .payload = strndup(payload, payload_len),
+    };
+
+    if (xQueueSend(global_dispatch_queue, &msg, 0) != pdTRUE) {
+        libiot_logf_error(TAG, "dropping sensor message for sensor path: %s", msg.path);
+        goto libsensor_dispatch_mqtt_message_send_failed;
+    }
+
+    return;
+
+libsensor_dispatch_mqtt_message_send_failed:
+    free(msg.path);
+    free(msg.payload);
 }
